a14737e707c2468cbf8452a650418699ed48f726
Bivone59 <odhiamborapolo124@gmail.com>
2023-06-17 05:16:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio.h> 
-/* *
- *main - function
+#include <stdio.h>
+/**
+ * main - function
  *
  * Return: 0 successful
- /*
+ */
 
- int main {void}
- {
- puts("Programming is like building a multilingual puzzle")
- return (0);
- }
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
