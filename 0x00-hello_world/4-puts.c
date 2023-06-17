c0dbd1d1d50faa8567d42dbfc981aaccfdca0411
Bivone59 <odhiamborapolo124@gmail.com>
2023-06-17 03:41:58 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
  *main - function
  *
  * Return: 0 successful
- 
+ /*
+
  int main {void}
  {
-    puts("\"Programming is like building a multilingual puzzle")
-    return (0);
-    }
+ puts("Programming is like building a multilingual puzzle")
+ return (0);
+ }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * *main - Thread printing a line using print f function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
